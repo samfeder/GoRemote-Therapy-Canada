e7cc8b1f993aa7d18b27835b5f34f9ca424b883a
Sam Feder <sam.feder1@gmail.com>
2020-03-26 15:15:49 -0400

    remove github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -61,13 +60,13 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                About Me
               </Link>
               <Link className="navbar-item" to="/outline">
                 Outline
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Articles
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
@@ -75,18 +74,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
